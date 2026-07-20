96349d95e621f36f56b9c04f2d88e7264ca45b48
Pedro Vitor <ppetrilii96@gmail.com>
2026-07-19 21:03:36 -0300

    feat: implement ThemeToggle component with dark mode functionality and add utility function for class merging

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { useState } from "react";
-import { Sun, Moon } from 'lucide-react';
+import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
 export default function ThemeToggle() {
-    const [isDarkMode, setIsDarkMode] = useState(true)
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-    return (
-        <button onClick={() => setIsDarkMode(!isDarkMode)} className="cursor-pointer">{
-            isDarkMode ? <Moon color="#90D5FF"/> : <Sun color="#90D5FF"/>
-        }</button>
-    );
+  useEffect(() => {
+    isDarkMode
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [isDarkMode]);
+
+  return (
+    <button
+      onClick={() => setIsDarkMode(!isDarkMode)}
+      className="cursor-pointer"
+    >
+      {isDarkMode ? <Moon color="#F2EFE3" /> : <Sun color="#F2EFE3" />}
+    </button>
+  );
 }
